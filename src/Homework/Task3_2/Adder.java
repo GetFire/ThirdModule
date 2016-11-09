89356f88305c0927e1cc1dc01ec15716bfad6f8b
Denis Azhymov <azhimov93@gmail.com>
2016-11-09 12:01:54 +0200

    init

--- src/Homework/Task3_2/Adder.java
+++ src/Homework/Task3_2/Adder.java
@@ -4,8 +4,18 @@ public class Adder extends Arithmetic {
     public boolean chek (Integer a, Integer b){
         boolean c=false;
         if(a>=b) {
-            boolean c = true;
+            c = true;
         }
         return c;
     }
+// Для провери создадим новый объект
+// _______________________________________________________
+
+
+//    public static void main(String[] args) {
+//        Adder You=new Adder();
+//        System.out.println(You.chek(12,10));
+//        System.out.println(You.arr(10,12));
+//
+//    }
 }
