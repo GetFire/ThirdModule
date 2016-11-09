93d229a35be95d006cb9af22d63754e9acf469f4
Denis Azhymov <azhimov93@gmail.com>
2016-11-09 12:51:31 +0200

    init

---
+++ src/Homework/Task3_3/CollegeStudent.java
@@ -0,0 +1,22 @@
+package Homework.Task3_3;
+
+
+public class CollegeStudent extends Student {
+    String collegeName;
+    int rating;
+    long id;
+
+    public CollegeStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public CollegeStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public CollegeStudent(String collegeName, int rating, long id) {
+        this.collegeName = collegeName;
+        this.rating = rating;
+        this.id = id;
+    }
+}
