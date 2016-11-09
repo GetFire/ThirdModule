a8a1d51a62744f3e0e37c6925e9bcd8f7ae1e4c5
Denis Azhymov <azhimov93@gmail.com>
2016-11-09 11:53:58 +0200

    init

--- src/Homework/Task3_2/Adder.java
+++ src/Homework/Task3_2/Adder.java
@@ -1,4 +1,11 @@
 package Homework.Task3_2;
 
 public class Adder extends Arithmetic {
+    public boolean chek (Integer a, Integer b){
+        boolean c=false;
+        if(a>=b) {
+            boolean c = true;
+        }
+        return c;
+    }
 }
