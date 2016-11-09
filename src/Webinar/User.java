4ebedbe21ce6b0c3e040f895843da7e9cedaa5bd
Denis Azhymov <azhimov93@gmail.com>
2016-11-09 09:01:14 +0200

    init

--- src/Webinar/User.java
+++ src/Webinar/User.java
@@ -2,16 +2,31 @@ package Webinar;
 
 public class User {
 
-    String name;
-    int age;
+     String name;
+     int age;
     String company;
+    private long id;
+
+    // default constructor
+    public User() {
+
+    }
+
+
+    // custom constructor
+    public User(long id, String name, int age, String company) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.company = company;
+    }
 
     void speak() {
         System.out.println("I am speaking");
     }
 
     void sendEmail() {
-
     }
 
 }
--- src/Webinar/UserMain.java
+++ src/Webinar/UserMain.java
@@ -2,9 +2,34 @@ package Webinar;
 
 public class UserMain {
     public static void main(String[] args) {
+
+
         User user = new User();
+        user.name = "Vitya";
+        System.out.println(user.name);
+        System.out.println(user.age);
+        ;
+        System.out.println(user.company);
+        System.out.println();
+
+        User user1 = new User(1001, "Denis", 10, "AVK");
+        User user2 = new User(1000, "Denis", 10, "AVK");
+        System.out.println(user1);
+
+        System.out.println(user1.name);
+        System.out.println(user1.age);
+
+        System.out.println(user1.company);
+        user1.name = "Eminem";
+        System.out.println(user1.name);
+        user.id = 1001;
+
+
+        user.sendEmail();
+    }
 
 
     }
+
 
 }
